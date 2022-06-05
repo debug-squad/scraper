40c5879595dbe20f09ae9385906a77e045021c6f
Jan Wolf <jan.wolf@student.um.si>
2022-06-05 18:07:20 +0200

    DEB-77 Event data

--- main.py
+++ main.py
@@ -22,7 +22,33 @@ def pridobi_podatke():
     items = elem_list.find_elements(By.XPATH, '//a[@data-itemtype= "catEvent"]')
 
     links = [item.get_attribute("href") for item in items]
-    print(links)
+    for site_url in links:
+        browser.get(site_url)
+        (browser.page_source).encode('utf-8')
+        vsebina = browser.find_element(By.CSS_SELECTOR,"div.col-lg-7, div.col-lg-6")
+        naslov = vsebina.find_element(By.XPATH, '//h2[@class="heading__title"]').text
+        try:
+            opis = vsebina.find_element(By.TAG_NAME, "p").text
+        except:
+            opis = "NULL"
+        try:
+            image = browser.find_element(By.CSS_SELECTOR, 'div.col-lg-5.event-image')
+            slika_url = image.find_element(By.TAG_NAME, "img")
+            url = slika_url.get_attribute("src")
+        except:
+            image = "NULL"
+            slika_url = "NULL"
+            url="NULL"   
+        podatki = vsebina.find_elements(By.XPATH, '//div[@class="featured-content__text"]')
+        podatki_array = []
+        for p in podatki:
+            podatki_array.append(p.text)
+
+        #
+        #
+        #
+        
+        print(podatki_array)
 
     browser.close()
 
